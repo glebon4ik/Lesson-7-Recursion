0a0ffb7e72ecf0e8fa60b865a790a86d8fc824b0
Gleb <gricay.gleb@mail.ru>
2024-06-02 12:44:46 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Элементы массива в обратном порядке:");
+PrintArrayReverse(array, array.Length - 1);
+
+void PrintArrayReverse(int[] array, int index)
+{
+
+    if (index < 0)
+    {
+        return;
+    }
+
+
+    Console.Write(array[index] + " ");
+
+
+    PrintArrayReverse(array, index - 1);
+}
+
